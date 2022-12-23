7bca04562157fcaac2a89814521066c873c56c9e
KombinatorVit <slonik0775@gmail.com>
2022-12-23 05:49:58 +0200

    restart

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { handleValidationErrors, checkAuth } from './utils/index.js';
 import { UserController, PostController } from './controllers/index.js';
 
 mongoose
-  .connect('mongodb+srv://admin:user104@cluster0.kzu4nlc.mongodb.net/?retryWrites=true&w=majority')
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
 
@@ -61,7 +61,7 @@ app.patch(
   PostController.update,
 );
 
-app.listen( 4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
     return console.log(err);
   }
