078da1db089394753fa79617a83c6a16a95a3a1f
KombinatorVit <slonik0775@gmail.com>
2023-03-22 23:48:43 +0200

    update

--- index.js
+++ index.js
@@ -5,32 +5,32 @@ import cors from 'cors';
 
 import mongoose from 'mongoose';
 
-import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
+import {registerValidation, loginValidation, postCreateValidation} from './validations.js';
 
-import { handleValidationErrors, checkAuth } from './utils/index.js';
+import {handleValidationErrors, checkAuth} from './utils/index.js';
 
-import { UserController, PostController } from './controllers/index.js';
+import {UserController, PostController} from './controllers/index.js';
 
 mongoose
-  .connect('cсылка с моего кластеря в базе данных')
-  .then(() => console.log('DB ok'))
-  .catch((err) => console.log('DB error', err));
+    .connect('mongodb+srv://vit:1111@blogbek.okvvup7.mongodb.net/blog?retryWrites=true&w=majority')
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB error', err));
 
 const app = express();
 
 const storage = multer.diskStorage({
-  destination: (_, __, cb) => {
-    if (!fs.existsSync('uploads')) {
-      fs.mkdirSync('uploads');
-    }
-    cb(null, 'uploads');
-  },
-  filename: (_, file, cb) => {
-    cb(null, file.originalname);
-  },
+    destination: (_, __, cb) => {
+        if (!fs.existsSync('uploads')) {
+            fs.mkdirSync('uploads');
+        }
+        cb(null, 'uploads');
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    },
 });
 
-const upload = multer({ storage });
+const upload = multer({storage});
 
 app.use(express.json());
 app.use(cors());
@@ -41,9 +41,9 @@ app.post('/auth/register', registerValidation, handleValidationErrors, UserContr
 app.get('/auth/me', checkAuth, UserController.getMe);
 
 app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-  res.json({
-    url: `/uploads/${req.file.originalname}`,
-  });
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    });
 });
 
 app.get('/tags', PostController.getLastTags);
@@ -54,17 +54,17 @@ app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
 app.patch(
-  '/posts/:id',
-  checkAuth,
-  postCreateValidation,
-  handleValidationErrors,
-  PostController.update,
+    '/posts/:id',
+    checkAuth,
+    postCreateValidation,
+    handleValidationErrors,
+    PostController.update,
 );
 
-app.listen(4444, (err) => {
-  if (err) {
-    return console.log(err);
-  }
+app.listen(4447, (err) => {
+    if (err) {
+        return console.log(err);
+    }
 
-  console.log('Server OK');
+    console.log('Server OKeyyyy');
 });
